442b91e140e5d777fc7127a62fd348d8827dd24f
berger.damian95@gmail.com <berger.damian95@gmail.com>
2020-09-29 21:03:04 +0200

    initial commit

---
+++ src/main/java/pl/ujbtrinity/devplatform/DevplatformApplication.java
@@ -0,0 +1,13 @@
+package pl.ujbtrinity.devplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevplatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevplatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/ujbtrinity/devplatform/DevplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ujbtrinity.devplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
