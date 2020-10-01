4acbe528a982ffafc894b30c6fc8dac121e29999
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-10-01 18:46:33 +0200

    InitTechnology service, creating most popular programming languages

---
+++ src/main/java/pl/ujbtrinity/devplatform/fixture/InitTechnologies.java
@@ -0,0 +1,45 @@
+package pl.ujbtrinity.devplatform.fixture;
+
+import org.springframework.stereotype.Service;
+import pl.ujbtrinity.devplatform.entity.Technology;
+import pl.ujbtrinity.devplatform.service.impl.TechnologyServiceImpl;
+
+@Service
+public class InitTechnologies {
+
+    public void init(String technologyName) {
+        Technology technology = new Technology();
+        technology.setName(technologyName);
+        technologyService.createTechnology(technology);
+    }
+
+    private final TechnologyServiceImpl technologyService;
+
+    public InitTechnologies(TechnologyServiceImpl technologyService) {
+        this.technologyService = technologyService;
+    }
+
+    public void intiTechnologies() {
+        init("Python");
+        init("JavaScript");
+        init("Java");
+        init("C#");
+        init("C");
+        init("C++");
+        init("R");
+        init("Swift");
+        init("PHP");
+        init("Dart");
+        init("Kotlin");
+        init("MATLAB");
+        init("Perl");
+        init("Ruby");
+        init("Rust");
+        init("Scala");
+        init("Elixir");
+        init("Clojure");
+        init("WebAssembly");
+        init("Assembly");
+        init("HTML/CSS");
+    }
+}
