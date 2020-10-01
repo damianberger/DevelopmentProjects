131bfc23f3bf6ac8df351810f72670d2820f0025
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-10-01 17:57:59 +0200

    Technology repository, service and his implementation with CRUD operations without delete operation

---
+++ src/main/java/pl/ujbtrinity/devplatform/service/impl/TechnologyServiceImpl.java
@@ -0,0 +1,40 @@
+package pl.ujbtrinity.devplatform.service.impl;
+
+import org.springframework.stereotype.Service;
+import pl.ujbtrinity.devplatform.entity.Technology;
+import pl.ujbtrinity.devplatform.repository.TechnologyRepository;
+import pl.ujbtrinity.devplatform.service.TechnologyService;
+
+import java.util.List;
+
+@Service
+public class TechnologyServiceImpl implements TechnologyService {
+
+    private final TechnologyRepository technologyRepository;
+
+    public TechnologyServiceImpl(TechnologyRepository technologyRepository) {
+        this.technologyRepository = technologyRepository;
+    }
+
+    @Override
+    public void createTechnology(Technology technology) {
+        technologyRepository.save(technology);
+    }
+
+    @Override
+    public void updateTechnology(Technology technology) {
+        Technology techFromDB = technologyRepository.getOne(technology.getId());
+        techFromDB.setName(technology.getName());
+        technologyRepository.save(techFromDB);
+    }
+
+    @Override
+    public List<Technology> FindAll() {
+        return technologyRepository.findAll();
+    }
+
+    @Override
+    public Technology readTechnology(long id) {
+        return technologyRepository.getOne(id);
+    }
+}
