4def2ede08e04eb8dd0c8bee5f3eef9f83481d48
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-10-13 13:09:51 +0200

    added other tokens to PurgeTask

--- src/main/java/pl/ujbtrinity/devplatform/service/impl/TokenPurgeTask.java
+++ src/main/java/pl/ujbtrinity/devplatform/service/impl/TokenPurgeTask.java
@@ -2,6 +2,9 @@ package pl.ujbtrinity.devplatform.service.impl;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import pl.ujbtrinity.devplatform.repository.ChangeMailTokenRepository;
+import pl.ujbtrinity.devplatform.repository.ChangePasswordTokenRepository;
+import pl.ujbtrinity.devplatform.repository.ResetPasswordTokenRepository;
 import pl.ujbtrinity.devplatform.repository.VerificationTokenRepository;
 
 import javax.transaction.Transactional;
@@ -13,9 +16,15 @@ import java.util.Date;
 public class TokenPurgeTask {
 
     private final VerificationTokenRepository verificationTokenRepository;
+    private final ChangePasswordTokenRepository changePasswordTokenRepository;
+    private final ChangeMailTokenRepository changeMailTokenRepository;
+    private final ResetPasswordTokenRepository resetPasswordTokenRepository;
 
-    public TokenPurgeTask(VerificationTokenRepository verificationTokenRepository) {
+    public TokenPurgeTask(VerificationTokenRepository verificationTokenRepository, ChangePasswordTokenRepository changePasswordTokenRepository, ChangeMailTokenRepository changeMailTokenRepository, ResetPasswordTokenRepository resetPasswordTokenRepository) {
         this.verificationTokenRepository = verificationTokenRepository;
+        this.changePasswordTokenRepository = changePasswordTokenRepository;
+        this.changeMailTokenRepository = changeMailTokenRepository;
+        this.resetPasswordTokenRepository = resetPasswordTokenRepository;
     }
 
     @Scheduled(cron = "* * 11 * * *")
@@ -23,5 +32,8 @@ public class TokenPurgeTask {
         Date now = Date.from(Instant.now());
 
         verificationTokenRepository.deleteAllExpiredSince(now);
+        changePasswordTokenRepository.deleteAllExpiredSince(now);
+        changeMailTokenRepository.deleteAllExpiredSince(now);
+        resetPasswordTokenRepository.deleteAllExpiredSince(now);
     }
 }
